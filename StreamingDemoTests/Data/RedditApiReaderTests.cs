a4c444776e2b6c2ae5ba79ff6feec845573afdc1
AlexReff <Alex.Reff@gmail.com>
2023-02-22 18:09:17 -0600

    Feature/visualization features 1 (#3)
    * Add tests proj
    
    * Cleanup & updated sln
    
    ---------
    
    Co-authored-by: Alex Reff <alex-reff@am.com>

---
+++ StreamingDemoTests/Data/RedditApiReaderTests.cs
@@ -0,0 +1,11 @@
+namespace StreamingDemo.Tests.Data
+{
+    public class RedditApiReaderTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ StreamingDemoTests/Hubs/RedditHubTests.cs
@@ -0,0 +1,11 @@
+namespace StreamingDemo.Tests.Data
+{
+    public class RedditHubTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
